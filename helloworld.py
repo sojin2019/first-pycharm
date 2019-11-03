4c6cd47c3cb5dc3a6bf0723dd003c9359c88d0a9
sojin2019 <56855153+sojin2019@users.noreply.github.com>
2019-11-04 00:10:27 +0900

    setting completed! wow

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,10 @@
-message1 = "Hello!"
-message2 = "This is your first project suing pycharm"
+message1 = "Hello!  "
+message2 = "This is my first project using pycharm."
 
-final_msg = message1.title()
-message2.title()
 
-print(message1)
+final_msg = message1.title()+message2.title()
+
+
+print(final_msg)
+
+print("Setting completed!! :)")
