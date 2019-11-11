0b38968133f5f4162f015c91083eabf8a1e9ba74
sojin2019 <56855153+sojin2019@users.noreply.github.com>
2019-11-11 23:30:33 +0900

    Update inputWindow.py
    if and else

--- inputWindow.py
+++ inputWindow.py
@@ -3,11 +3,19 @@ import mainWindow
 import inputSchedule
 from PyQt5.QtWidgets import *
 
-
+ from datetime import datetime
+    now = datetime.now()
+    
 if __name__ == "__main__":
     mw = mainWindow.mw('addition test')
     mw
 
-    inputSchedule.InputSchedule('20191110','2345', 'Coding', 'everyday for 30 days' )
+    if now.day < 10:
+        in_item_date = str(now.year) + str(now.month) + "0" + str(now.day)
+    else:
+        in_item_date = str(now.year)+str(now.month)+str(now.day)
+    
+    
+    inputSchedule.InputSchedule(in_item_date,'2345', 'Coding', 'everyday for 30 days' )
 
 
