ab5cffc55868cdb79f2bd67dd576a532cee0e4c8
sojin <coolyesso@naver.com>
2019-11-22 23:45:41 +0900

    ui success

--- dialogExample.py
+++ dialogExample.py
@@ -5,28 +5,37 @@ from PyQt5 import uic
 
 #UI파일 연결
 #단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-form_class = uic.loadUiType("pushbuttonTest.ui")[0]
+#form_class = uic.loadUiType("pushbuttonTest.ui")[0]
 
-
-class dialogExample(QMainWindow, form_class):
+class dialogExample(QMainWindow):
 
     def __init__(self):
         #Add button
         super().__init__()
         print('test3')
-        self.btnctrl()
-        print('test4')
 
-    def btnctrl(self):
-        btn = QPushButton(self)
+        # x,y,width,height
+        self.setGeometry(300,300,350,150)
+        # window title
+        self.setWindowTitle("Button Test")
+
+        btn_1 = QPushButton('Click', self)
+        btn_1.setGeometry(50, 80, 120, 40)
+        btn_2 = QPushButton('Exit', self)
+        btn_2.setGeometry(190,80, 120, 40)
+
+
+        btn_1.clicked.connect(self.btnfc)
         # Quit Button
-        btn_2 = QPushButton(self)
-        
-        btn.clicked.connect(self.btnfc)
         btn_2.clicked.connect(QCoreApplication.instance().quit)
 
+
+
     def btnfc(self):
-        print('exit')
+        print('hello')
+        self.close()
+        QCoreApplication.instance().quit
+
 
 
 
