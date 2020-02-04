fdb6b2918f367812717c58529af55d2a5941d742
sojin <coolyesso@naver.com>
2020-02-04 23:21:47 +0900

    Input and Tk test

---
+++ inputExample.py
@@ -0,0 +1,7 @@
+
+print('Please input your name', end="")
+name = input();
+print("Name : {0}, type : {1}".format(name,type(name)))
+name = input('Please input your favorite city');
+
+name = input('아무것도 입력하지 말고 EOF(Ctrl+D 또는 Ctrl+Z+Enter)를 입력해보세요');
---
+++ labelExample.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window1 = tk.Tk()
+window1.title('Calculator')
+
+
+lb1 = tk.Label(window1, text='1st Label')
+lb1.pack()
+
+window1.mainloop()
